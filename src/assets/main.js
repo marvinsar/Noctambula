09a59f501dec503ae36855fac4dfcebdc10f4fe5
Marvinsar <marvinsarsito@gmail.com>
2023-05-22 18:50:14 -0500

    se agrego el archivo main.js, packge.js, tambien se agrego API de videos de youtube

---
+++ src/assets/main.js
@@ -0,0 +1,47 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCBVjMGOIkavEAhyqpxJ73Dw&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content')
+
+const options = {
+  method: 'GET',
+  headers: {
+    'content-type': 'application/octet-stream',
+    'X-RapidAPI-Key': '578ce70bdcmshd3c731a172b0077p1375bfjsn72da463daca3',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+
+async function fetchData (urlApi) {
+  const response = await fetch(urlApi, options)
+  const data = await response.json()
+  return data
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API)
+    const view = `
+    ${videos.items.map(video => `
+      <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">  
+        <div class="group relative">
+          <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div> 
+        
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm dark:text-slate-300">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+      
+    `).slice(0, 4).join('')}
+    `
+    content.innerHTML = view
+  } catch (error) {
+    console.log(error)
+  }
+// eslint-disable-next-line semi
+})();
